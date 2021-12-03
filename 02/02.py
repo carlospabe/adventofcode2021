c24e26bfee5056c2d5f73d07ba6f2eafb2248868
carlospabe <46899099+carlospabe@users.noreply.github.com>
2021-12-03 09:03:22 +0100

    day 2 completed

---
+++ 02/02.py
@@ -0,0 +1,53 @@
+def get_instructions():
+    with open("02_input.txt", 'r', encoding='utf-8-sig') as f:
+        lines = f.read().splitlines()
+    
+    instructions = list()
+    for line in lines:
+        command, units = line.split()
+        instruction = dict()
+        instruction['command'] = str(command)
+        instruction['units'] = int(units)
+        instructions.append(instruction)
+    
+    return instructions
+
+
+def part_1(instructions):
+    position = {
+        'horizontal': 0,
+        'depth': 0
+    }
+    for inst in instructions:
+        if inst['command'] == "forward":
+            position['horizontal'] += inst['units']
+        elif inst['command'] == "up":
+            position['depth'] -= inst['units']
+        else:  # inst['command'] == "down"
+            position['depth'] += inst['units']
+            
+    return position['depth'] * position['horizontal']
+
+
+def part_2(instructions):
+    position = {
+        'horizontal': 0,
+        'depth': 0,
+        'aim': 0
+    }
+    for inst in instructions:
+        if inst['command'] == "forward":
+            position['horizontal'] += inst['units']
+            position['depth'] += position['aim'] * inst['units']
+        elif inst['command'] == "up":
+            position['aim'] -= inst['units']
+        else:  # inst['command'] == "down"
+            position['aim'] += inst['units']
+            
+    return position['depth'] * position['horizontal']
+
+
+instructions = get_instructions()
+
+print(part_1(instructions))
+print(part_2(instructions))
